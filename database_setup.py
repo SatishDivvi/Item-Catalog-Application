e5656305ad1498a5241358f2f8f28f89c7f3eb8d
Divvi Naga Venkata Satish <divvinagavenkata.satish69@gmail.com>
2019-05-09 13:11:57 -0700

    Update nullable to False

--- database_setup.py
+++ database_setup.py
@@ -16,7 +16,7 @@ class items(Base):
     __tablename__ = 'items'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    description = Column(String(250), nullable=True)
+    description = Column(String(250), nullable=False)
     category_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(Category)
 
