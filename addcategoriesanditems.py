6f67f3376333f4db9552457eb93e783fa6115a88
Divvi Naga Venkata Satish <divvinagavenkata.satish69@gmail.com>
2019-05-09 16:45:25 -0700

    Create sqlite engine

--- addcategoriesanditems.py
+++ addcategoriesanditems.py
@@ -2,3 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from database_setup import Category, Base, Item
+
+engine = create_engine('sqlite:///catalog.db')
+# Bind the engine to the metadata of the Base class so that the
+# declaratives can be accessed through a DBSession instance
+Base.metadata.bind = engine
