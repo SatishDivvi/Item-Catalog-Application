96929beaf30bf4f625e9ada4bab73e4364aa63cb
Divvi Naga Venkata Satish <divvinagavenkata.satish69@gmail.com>
2019-05-09 13:02:44 -0700

    Create Items Class

--- database_setup.py
+++ database_setup.py
@@ -9,3 +9,12 @@ class Category(Base):
     __tablename__ = 'category'
     id = Column(Integer, primary_key = True)
     name = Column(String(250), nullable = False)
+
+class items(Base):
+    __tablename__ = 'items'
+    id = Column(Integer, primary_key = True)
+    name = Column(String(250), nullable = True)
+    description = Column(String(250), nullable=True)
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+
