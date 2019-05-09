b41859c8fb7d3a51abe683710d25d77b71d9ef8f
Divvi Naga Venkata Satish <divvinagavenkata.satish69@gmail.com>
2019-05-09 13:01:55 -0700

    Create Category Class

--- database_setup.py
+++ database_setup.py
@@ -2,3 +2,10 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class Category(Base):
+    __tablename__ = 'category'
+    id = Column(Integer, primary_key = True)
+    name = Column(String(250), nullable = False)
