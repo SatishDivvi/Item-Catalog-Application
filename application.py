a7448680fb1fb1e0c416797627f5bb0491d8f9bb
Divvi Naga Venkata Satish <divvinagavenkata.satish69@gmail.com>
2019-05-09 21:04:30 -0700

    Make PEP8 Compliant

--- application.py
+++ application.py
@@ -46,6 +46,7 @@ def editCategory(category_id):
     else:
         return render_template('editCategory.html', category = editCategory)
 
+
 @app.route('/catalog/<int:category_id>/delete/', methods=['GET', 'POST'])
 def deleteCategory(category_id):
     DBSession = sessionmaker(bind=engine)
